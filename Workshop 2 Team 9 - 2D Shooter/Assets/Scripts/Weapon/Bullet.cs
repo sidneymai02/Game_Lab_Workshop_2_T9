93398b281bd5ffc516c610859ba295b365f7ff1f
sidneymai02 <sidneymai2010@yahoo.com>
2022-10-15 14:06:21 -0700

    Added Scripts
    Added in scripts for weapon mechanics, enemy behavior, and player movement

---
+++ Workshop 2 Team 9 - 2D Shooter/Assets/Scripts/Weapon/Bullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public int damage = 5;
+    public Rigidbody2D rb;
+    public GameObject impactEffect;
+    
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D (Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
